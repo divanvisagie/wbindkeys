ba1296c8e84989a08c0e245f03a1466b3cccfd9d
Divan Visagie <me@divanv.com>
2024-07-09 19:12:04 +0200

    Prints bound lua config on key press
    We now print a value that was passed in via a lua binding

--- src/main.rs
+++ src/main.rs
@@ -3,37 +3,32 @@ use input::{Event, Libinput, LibinputInterface};
 use mlua::{Function, Lua, Table};
 
 use libc::{O_RDONLY, O_RDWR, O_WRONLY};
+use std::collections::HashMap;
 use std::fs::{File, OpenOptions};
 use std::os::unix::{fs::OpenOptionsExt, io::OwnedFd};
 use std::path::Path;
+use std::sync::{Arc, Mutex};
 
 struct Interface;
 
 impl LibinputInterface for Interface {
     fn open_restricted(&mut self, path: &Path, flags: i32) -> Result<OwnedFd, i32> {
-        OpenOptions::new()
+        let file = OpenOptions::new()
             .custom_flags(flags)
-            .read((flags & O_RDONLY != 0) | (flags & O_RDWR != 0))
-            .write((flags & O_WRONLY != 0) | (flags & O_RDWR != 0))
-            .open(path)
-            .map(|file| file.into())
-            .map_err(|err| err.raw_os_error().unwrap())
+            .read((flags & O_RDONLY != 0) || (flags & O_RDWR != 0))
+            .write((flags & O_WRONLY != 0) || (flags & O_RDWR != 0))
+            .open(path);
+
+        match file {
+            Ok(f) => Ok(f.into()),
+            Err(err) => Err(err.raw_os_error().unwrap_or(-1)),
+        }
     }
+
     fn close_restricted(&mut self, fd: OwnedFd) {
         drop(File::from(fd));
     }
 }
-
-fn key_to_string(key: u32) -> &'static str {
-    match key {
-        30 => "A",
-        31 => "B",
-        56 => "LeftAlt",
-        125 => "LeftMod",
-        _ => "Unknown",
-    }
-}
-
 enum Keys {
     A = 30,
     B = 31,
@@ -46,29 +41,44 @@ fn main() {
     input.udev_assign_seat("seat0").unwrap();
 
     let lua = Lua::new();
-    
-    let r = lua.create_function(|_, (binding, target): (String, Function)| {
-        println!("Binding key: {:?}", binding);
-        println!("Target: {:?}", target);
-        target.call::<_, ()>(()).unwrap();
-        Ok(())
-    }).unwrap();
+    let actions: HashMap<String, String> = HashMap::new();
+    let lua = Arc::new(Mutex::new(lua));
+    let actions = Arc::new(Mutex::new(actions));
 
-    lua.globals().set("bind", r).unwrap();
-    // Sample Lua script
-    let script = r#"
-        -- Run a Lua function when calling
-        bind("alt+T", function()
-            print("Alt key pressed")
-            local result = os.execute("ls -l")
-            print("Result: ", result)
-        end) 
-        print("lol");
-    "#;
-    let lua_ctx = lua.load(script);
-    let result = lua_ctx.exec(); 
-    println!("Result output {:?}", result);
-   
+    {
+        let lua = Arc::clone(&lua);
+        let actions = Arc::clone(&actions);
+
+        let lock = lua.lock().unwrap();
+        let r = lock
+            .create_function(move |_, (binding, target): (String, String)| {
+                println!("Binding key: {:?}", binding);
+                println!("Target: {:?}", target);
+                let mut actions_lock = actions.lock().unwrap();
+                actions_lock.insert(binding, target);
+                Ok(())
+            })
+            .unwrap();
+        lock.globals().set("bind", r).unwrap();
+    }
+
+    {
+        let lua = lua.clone();
+        let actions = actions.clone();
+        let script = r#"
+            bind("Alt+A", "test")
+            print("lol")
+        "#;
+        let lock = lua.lock().unwrap();
+        let result = lock.load(script).exec();
+        println!("Result output {:?}", result);
+
+        //execute all actions
+        for (key, action) in actions.lock().unwrap().iter() {
+            println!("Key: {:?}, Action: {:?}", key, action);
+        }
+    }
+
     let mut active_keys = Vec::new();
     loop {
         input.dispatch().unwrap();
@@ -77,19 +87,22 @@ fn main() {
                 Event::Keyboard(kb_event) => {
                     let key = kb_event.key();
                     let state = kb_event.key_state();
-                    println!("Keyboard event: key {:?}, state {:?}", key, state);
-                    // Here, you can add logic to handle specific keys and states
+
                     if key == Keys::LeftAlt as u32 && state == KeyState::Pressed {
                         active_keys.push(key);
-                        println!("Alt key pressed");
                     }
                     if key == Keys::LeftAlt as u32 && state == KeyState::Released {
-                        println!("Alt key released");
                         active_keys.clear();
                     }
 
-                    if active_keys.contains(&(Keys::LeftAlt as u32)) && key == Keys::A as u32 {
-                        println!("Alt + A pressed");
+                    if active_keys.contains(&(Keys::LeftAlt as u32))
+                        && key == Keys::A as u32
+                        && state == KeyState::Pressed
+                    {
+
+                        if let Some(action) = actions.lock().unwrap().get("Alt+A") {
+                            println!("Action: {:?}", action);
+                        }
                     }
                 }
                 _ => {} // Ignore non-keyboard events
